d840f5b58017d8e3cb117e0df526c7c6d34692e4
Nicolas Sagnette <nicolas.sagnette@atinternet.com>
2021-03-30 12:12:09 +0200

    changes: bump version

--- ATInternetTracker/Sources/TechnicalContext.swift
+++ ATInternetTracker/Sources/TechnicalContext.swift
@@ -49,9 +49,9 @@ class TechnicalContext: NSObject {
     class var sdkVersion: String {
         get {
             #if os(watchOS) || os(tvOS)
-            return "1.20.0"
+            return "1.20.1"
             #else
-            return "2.23.0"
+            return "2.23.1"
             #endif
         }
     }
